6f2af5eb547313782cf7eb4ab0da85840f55cba9
Deon Odendaal <deon.odendaal@protonmail.com>
2020-04-04 13:11:20 +0100

    axios error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,24 +1,54 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "milton keynes"
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (error) {
+      setErrorMessage("Something went wrong");
+    }
+  };
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermSubmit={() => console.log("term was submitted: ", term)}
+        onTermSubmit={searchApi}
         onTermChange={newTerm => setTerm(newTerm)}
       />
+      {errorMessage ? (
+        <Text style={styles.errorTextStyle}>{errorMessage}</Text>
+      ) : null}
+      <Text style={styles.textStyles}>
+        We have found {results.length} results
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   textStyles: {
-    fontSize: 16
+    fontSize: 16,
+    marginHorizontal: 15
+  },
+  errorTextStyle: {
+    fontSize: 16,
+    margin: 15,
+    color: "red"
   }
 });
 
