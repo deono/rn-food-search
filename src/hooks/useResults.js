5394f4b9a392a43c947549aa19e07222c4dd2bb1
Deon Odendaal <deon.odendaal@protonmail.com>
2020-04-05 14:21:33 +0100

    data display in ResultsShowScreen

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -18,7 +18,7 @@ export default () => {
         params: {
           limit: 50,
           term: searchTerm,
-          location: "london"
+          location: "milton keynes"
         }
       });
       setResults(response.data.businesses);
--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, FlatList } from "react-native";
 import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ navigation }) => {
@@ -16,15 +16,56 @@ const ResultsShowScreen = ({ navigation }) => {
     getResult(id);
   }, []);
 
+  if (!result) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>ResultsShowScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.headingStyles}>{result.name}</Text>
+      <Text style={styles.subStyles}>
+        Rating: {result.rating}, Reviews: {result.review_count}
+      </Text>
+      <Text style={styles.addressStyles}>{result.location.address1}</Text>
+
+      <Text style={styles.addressStyles}>{result.location.city}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={photo => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.imageStyles} source={{ uri: item }} />;
+        }}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   // styles
+  container: {
+    margin: 15
+  },
+  imageStyles: {
+    height: 200,
+    width: 300,
+    marginTop: 15
+  },
+  headingStyles: {
+    fontSize: 18,
+    fontWeight: "bold"
+  },
+  subStyles: {
+    fontSize: 16
+  },
+  addressStyles: {
+    fontSize: 14,
+    color: "gray",
+    marginLeft: 10
+  }
 });
 
 export default ResultsShowScreen;
