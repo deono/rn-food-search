aab3f94f5f4962f9ec67564ef1c99bce2d6fc6e6
Deon Odendaal <deon.odendaal@protonmail.com>
2020-04-05 10:26:28 +0100

    display results in ResultsDetail

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -4,13 +4,26 @@ import { View, Image, Text, StyleSheet } from "react-native";
 const ResultsDetail = ({ result }) => {
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Image style={styles.imageStyles} source={{ uri: result.image_url }} />
+      <Text style={styles.nameStyles}>{result.name}</Text>
+      <Text>
+        Rating: {result.rating}, Reviews: {result.review_count}
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   // styles
+  imageStyles: {
+    width: 250,
+    height: 120,
+    borderRadius: 4
+  },
+  nameStyles: {
+    fontWeight: "bold",
+    fontSize: 16
+  }
 });
 
 export default ResultsDetail;
